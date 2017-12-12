8721408c25186196c7fd81077bb8feb3e85673d2
Venay <silver0snake@gmail.com>
2017-12-12 20:47:45 +0100

    Initial commit

---
+++ Assets/pongMouvment.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pongMouvment : MonoBehaviour {
+
+    public float speed = 10f;
+    public Rigidbody2D rb;
+    public Text scoreUI;
+
+    int score = 0;
+
+	// Use this for initialization
+	void Start () {
+        rb.AddForce(Vector2.up*speed);
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        score = score + 1;
+        scoreUI.text = score.ToString();
+
+    }
+}
---
+++ Assets/pongerRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pongerRotation : MonoBehaviour {
+
+    public float rotationSpeed = 40f;
+
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.forward, Time.deltaTime * rotationSpeed);
+	}
+
+    private void OnMouseUp ()
+    {
+        rotationSpeed = rotationSpeed * (-1);
+    }
+}
